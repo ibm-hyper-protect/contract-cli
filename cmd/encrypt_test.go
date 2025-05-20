da1b665bdcb4ef0b77a875873a02eb5e165bf1b1
Sashwat K <Sashwat.K@ibm.com>
2025-05-20 08:37:07 +0530

    feat: add testcase for contract expiry

--- cmd/encrypt_test.go
+++ cmd/encrypt_test.go
@@ -12,10 +12,18 @@ const (
 	sampleSignEncryptInputPath   = "../samples/contract.yaml"
 	sampleSignEncryptPrivatePath = "../samples/sign/private.pem"
 	sampleSignEncryptOutputPath  = "../build/signed-encrypt.yaml"
+
+	sampleContractExpiryPrivatePath = "../samples/contract-expiry/private.pem"
+	sampleContractExpiryCaCertPath  = "../samples/contract-expiry/personal_ca.crt"
+	sampleContractExpiryCaKeyPath   = "../samples/contract-expiry/personal_ca.pem"
+	sampleContractExpiryCsrPath     = "../samples/contract-expiry/csr.pem"
+	sampleContractExpiryExpiry      = "100"
+	sampleContractExpiryOutputPath  = "../build/signed-encrypt-contract-expiry.yaml"
 )
 
 var (
-	sampleSignEncryptCommand = []string{common.EncryptParamName, "--in", sampleSignEncryptInputPath, "--priv", sampleSignEncryptPrivatePath, "--out", sampleSignEncryptOutputPath}
+	sampleSignEncryptCommand    = []string{common.EncryptParamName, "--in", sampleSignEncryptInputPath, "--priv", sampleSignEncryptPrivatePath, "--out", sampleSignEncryptOutputPath}
+	sampleContractExpiryCommand = []string{common.EncryptParamName, "--contract-expiry", "--in", sampleSignEncryptInputPath, "--priv", sampleContractExpiryPrivatePath, "--cacert", sampleContractExpiryCaCertPath, "--cakey", sampleContractExpiryCaKeyPath, "--csr", sampleContractExpiryCsrPath, "--expiry", sampleContractExpiryExpiry, "--out", sampleContractExpiryOutputPath}
 )
 
 func TestEncryptCmdText(t *testing.T) {
@@ -25,6 +33,18 @@ func TestEncryptCmdText(t *testing.T) {
 	rootCmd.SetErr(buf)
 
 	rootCmd.SetArgs(sampleSignEncryptCommand)
+	err := encryptCmd.Execute()
+
+	assert.NoError(t, err)
+}
+
+func TestEncryptCmdTextContractExpiry(t *testing.T) {
+	// Capture output
+	buf := new(bytes.Buffer)
+	rootCmd.SetOut(buf)
+	rootCmd.SetErr(buf)
+
+	rootCmd.SetArgs(sampleContractExpiryCommand)
 	err := encryptCmd.Execute()
 
 	assert.NoError(t, err)
