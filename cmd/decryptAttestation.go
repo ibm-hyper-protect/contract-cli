dc1c38ac433d45c12ba09b2a8e1cb1f62c78c90a
Vikas Sharma <vikas.sharma24@ibm.com>
2025-12-07 22:36:11 +0530

    refactor: decrypt attestation section

--- cmd/decryptAttestation.go
+++ cmd/decryptAttestation.go
@@ -16,36 +16,29 @@
 package cmd
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/ibm-hyper-protect/contract-go/v2/attestation"
+	"github.com/ibm-hyper-protect/contract-cli/lib/decryptAttestation"
 	"github.com/spf13/cobra"
-
-	"github.com/ibm-hyper-protect/contract-cli/common"
-)
-
-const (
-	successMessageDecryptAttestation = "Successfully decrypted attestation records"
 )
 
 // decryptAttestationCmd represents the decrypt-attestation command
 var decryptAttestationCmd = &cobra.Command{
-	Use:   common.DecryptAttestParamName,
-	Short: common.DecryptAttestParamShortDescription,
-	Long:  common.DecryptAttestParamLongDescription,
+	Use:   decryptAttestation.ParameterName,
+	Short: decryptAttestation.ParameterShortDescription,
+	Long:  decryptAttestation.ParameterLongDescription,
 	Run: func(cmd *cobra.Command, args []string) {
-		encAttestPath, privateKeyPath, decryptedAttestPath, err := validateInputDecryptedAttestation(cmd)
+		encAttestPath, privateKeyPath, decryptedAttestPath, err := decryptAttestation.ValidateInput(cmd)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		decryptedAttestationRecords, err := decryptAttestationRecords(encAttestPath, privateKeyPath)
+		decryptedAttestationRecords, err := decryptAttestation.DecryptAttestationRecords(encAttestPath, privateKeyPath)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		err = printDecryptAttestation(decryptedAttestationRecords, decryptedAttestPath)
+		err = decryptAttestation.PrintDecryptAttestation(decryptedAttestationRecords, decryptedAttestPath)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -56,66 +49,7 @@ var decryptAttestationCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(decryptAttestationCmd)
 
-	decryptAttestationCmd.PersistentFlags().String(common.FileInFlagName, common.DecryptAttestFileInDefaultPath, common.DecryptAttestFileInDescription)
-	decryptAttestationCmd.PersistentFlags().String(common.PrivateKeyFlagName, "", common.PrivateKeyFlagDescription)
-	decryptAttestationCmd.PersistentFlags().String(common.FileOutFlagName, "", common.DecryptAttestFlagDescription)
-}
-
-// validateInputDecryptedAttestation - function to validate decrypt-attestation inputs
-func validateInputDecryptedAttestation(cmd *cobra.Command) (string, string, string, error) {
-	encAttestPath, err := cmd.Flags().GetString(common.FileInFlagName)
-	if err != nil {
-		return "", "", "", err
-	}
-
-	privateKeyPath, err := cmd.Flags().GetString(common.PrivateKeyFlagName)
-	if err != nil {
-		return "", "", "", err
-	}
-
-	decryptedAttestPath, err := cmd.Flags().GetString(common.FileOutFlagName)
-	if err != nil {
-		return "", "", "", err
-	}
-
-	return encAttestPath, privateKeyPath, decryptedAttestPath, nil
-}
-
-// decryptAttestationRecords - function to decrypt attestation records
-func decryptAttestationRecords(encryptedAttestationRecordsPath, privateKeyPath string) (string, error) {
-	if !common.CheckFileFolderExists(encryptedAttestationRecordsPath) || !common.CheckFileFolderExists(privateKeyPath) {
-		log.Fatal("The path to encrypted attestation records file or private key doesn't exists")
-	}
-
-	encryptedChecksum, err := common.ReadDataFromFile(encryptedAttestationRecordsPath)
-	if err != nil {
-		return "", err
-	}
-
-	privateKey, err := common.ReadDataFromFile(privateKeyPath)
-	if err != nil {
-		return "", err
-	}
-
-	decryptedAttestationRecords, err := attestation.HpcrGetAttestationRecords(encryptedChecksum, privateKey)
-	if err != nil {
-		return "", err
-	}
-
-	return decryptedAttestationRecords, nil
-}
-
-// printDecryptAttestation - print decrypted attestation records or redirect to a file
-func printDecryptAttestation(decryptedAttestationRecords, decryptedAttestationPath string) error {
-	if decryptedAttestationPath != "" {
-		err := common.WriteDataToFile(decryptedAttestationPath, decryptedAttestationRecords)
-		if err != nil {
-			return err
-		}
-		fmt.Println(successMessageDecryptAttestation)
-	} else {
-		fmt.Println(decryptedAttestationRecords)
-	}
-
-	return nil
+	decryptAttestationCmd.PersistentFlags().String(decryptAttestation.InputFlagName, decryptAttestation.DecryptAttestFileInDefaultPath, decryptAttestation.DecryptAttestFileInDescription)
+	decryptAttestationCmd.PersistentFlags().String(decryptAttestation.PrivateKeyFlagName, "", decryptAttestation.PrivateKeyFlagDescription)
+	decryptAttestationCmd.PersistentFlags().String(decryptAttestation.OutputFlagName, "", decryptAttestation.DecryptAttestFlagDescription)
 }
